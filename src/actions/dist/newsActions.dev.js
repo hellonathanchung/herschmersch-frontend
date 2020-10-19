6af01475d429595637185430c08b3967ba8ecf6a
hellonathanchung <chung.nathanh@gmail.com>
2020-10-19 13:31:17 -0400

    updates date with moment

--- src/actions/dist/newsActions.dev.js
+++ src/actions/dist/newsActions.dev.js
@@ -7,7 +7,7 @@ exports.fetchNews = void 0;
 
 var _types = require("./types");
 
-var _reactMoment = _interopRequireDefault(require("react-moment"));
+var _moment = _interopRequireDefault(require("moment"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -15,10 +15,9 @@ var API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 
 var fetchNews = function fetchNews(symbol) {
   return function (dispatch) {
-    var date = (0, _reactMoment["default"])().format("DD-MM-YYYY hh:mm:ss");
-    console.lo(date); // let ticker = property passed down
-
-    var URL = "http://newsapi.org/v2/everything?q=".concat(symbol, "&from=2020-10-10&sortBy=publishedAt&apiKey=").concat(API_KEY);
+    var date = (0, _moment["default"])().format('YYYY-MM-DD');
+    console.log(reformattedDate);
+    var URL = "http://newsapi.org/v2/everything?q=".concat(symbol, "&from=").concat(date, "&sortBy=publishedAt&apiKey=").concat(API_KEY);
     debugger;
     console.log('fetching news...');
     fetch(URL).then(function (res) {
--- src/actions/newsActions.js
+++ src/actions/newsActions.js
@@ -1,15 +1,18 @@
 
 import {FETCH_NEWS} from './types'
+import moment from 'moment'
 let API_KEY = process.env.REACT_APP_NEWS_API_KEY
-import moment from 'react-moment';
+
+
 
 
 export const fetchNews = (symbol) => dispatch => {
-  
-let date =  moment().format("DD-MM-YYYY hh:mm:ss")
-  console.lo(date)
-  // let ticker = property passed down
-  let URL = `http://newsapi.org/v2/everything?q=${symbol}&from=2020-10-10&sortBy=publishedAt&apiKey=${API_KEY}` 
+
+  let date= moment().format('YYYY-MM-DD');
+
+
+  console.log(reformattedDate)
+  let URL = `http://newsapi.org/v2/everything?q=${symbol}&from=${date}&sortBy=publishedAt&apiKey=${API_KEY}` 
   debugger
   console.log('fetching news...')
   
