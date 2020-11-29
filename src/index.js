45272af82541c1e34a0ab02fcb7a741bd870ef80
Nathan Chung <nathan.h.chung@ekostrategiesllc.com>
2020-11-28 16:33:48 -0800

    untracked node_modules

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import {Provider} from 'react-redux'
 import { BrowserRouter as Router} from 'react-router-dom';
 import store from './store'
+import 'semantic-ui-css/semantic.min.css';
+
+
 
 
 
